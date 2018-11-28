052a9ae13fc25e46d94d399dfefcd9bf11a0d29a
Ariadna Acedo <ariadnaacedo@MacBook-Pro-de-Ariadna-2.local>
2018-11-28 18:46:04 +0100

    Rosa

--- seqClass.sh
+++ seqClass.sh
@@ -16,10 +16,6 @@ motif=$(echo $2 | tr a-z A-Z)
 if [[ -n $motif ]]; then
   echo -en "Motif search enabled: looking for motif '$motif' in sequence '$seq'... "
   if [[ $seq =~ $motif ]]; then
-    echo "Tulipan"
+    echo "Rosa"
   else
-<<<<<<< HEAD
-    echo "No hay Margaritas"
-=======
-  fi
 fi
