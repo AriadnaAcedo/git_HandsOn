c1602d14c470dca75cc118cdc63de4ffd09718a0
Ariadna Acedo <ariadnaacedo@MacBook-Pro-de-Ariadna-2.local>
2018-11-28 18:21:56 +0100

    Si hay margarita

--- seqClass.sh
+++ seqClass.sh
@@ -16,8 +16,8 @@ motif=$(echo $2 | tr a-z A-Z)
 if [[ -n $motif ]]; then
   echo -en "Motif search enabled: looking for motif '$motif' in sequence '$seq'... "
   if [[ $seq =~ $motif ]]; then
-    echo "FOUND"
+    echo "Tulipan"
   else
-    echo "Margarita"
+    echo "Si hay margarita"
   fi
 fi
